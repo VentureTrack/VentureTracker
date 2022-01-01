3e6e38936ed7429499257349de8566ec043d18dc
Farhan Rehman <37966672+FarhanRehman@users.noreply.github.com>
2021-12-31 22:01:09 -0500

    Frontend Redesign
    navbar, cards

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import { NavLink  } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 /* This example requires Tailwind CSS v2.0+ */
 import { Disclosure } from '@headlessui/react'
@@ -21,80 +21,28 @@ export default function Navbar() {
   ]
 
   return (
-    <>
-      <div className="min-h-full">
-        <Disclosure as="nav" className="bg-white">
-          {({ open }) => (
-            <>
-              <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                <div className="flex items-center justify-between h-16">
-                  
-                  <NavLink  to=''>
-                    <div className="flex items-center">
-                      <div className="flex-shrink-0">
-                          <img
-                            className="h-8 w-8"
-                            src={ logo }
-                            alt="CompoDom"
-                          />
-                      </div>
-                      <h1 class="ml-5 font-semibold text-gray-500 text-lg" style={{ color: 'black' }}>VentureTrack</h1>
-                    </div>
-                  </NavLink >
-                  
-                  <div>
-                    <div className="hidden md:block">
-                      <div className="ml-10 flex items-baseline space-x-5 justify-right">
-                        {navigation.map((item) => (
-                          <NavLink 
-                            key={item.name}
-                            exact to={item.href}
-                            activeClassName="text-black-500 border-b-4 border-blue-500"
-                            className="px-3 py-2 rounded-md font-semibold text-black-500 border-b-4 border-white hover:border-blue-500 transition duration-300"
-                          >
-                            {item.name}
-                          </NavLink >
-                        ))}
-                      </div>
-                    </div>
-                  </div>
+    <div clasName="">
+      <nav className="flex justify-center py-8 bg-gray-900">
+        
+        <div>
+          <h3 className="text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-tr from-green-300 via-blue-400 to-purple-600">VentureTrack</h3>
+        </div>
 
-                  <div className="-mr-2 flex md:hidden">
-                    {/* Mobile menu button */}
-                    <Disclosure.Button className="bg-blue-500 inline-flex items-center justify-center p-2 rounded-md text-black-500 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                      <span className="sr-only">Open main menu</span>
-                      {open ? (
-                        <XIcon className="block h-6 w-6" aria-hidden="true" />
-                      ) : (
-                        <MenuIcon className="block h-6 w-6" aria-hidden="true" />
-                      )}
-                    </Disclosure.Button>
-                  </div>
-                </div>
-              </div>
+        <div className="hidden md:flex space-x-8 mx-8 py-2 lg-flex">
+          { navigation.map(({ name, href }) => (
 
-              <Disclosure.Panel className="md:hidden">
-                <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-                  {navigation.map((item) => (
-                    <Disclosure.Button
-                      key={item.name}
-                      as="a"
-                      href={item.href}
-                      className={classNames(
-                        item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                        'block px-3 py-2 rounded-md text-base font-medium'
-                      )}
-                      aria-current={item.current ? 'page' : undefined}
-                    >
-                      {item.name}
-                    </Disclosure.Button>
-                  ))}
-                </div>
-              </Disclosure.Panel>
-            </>
-          )}
-        </Disclosure>
-      </div>
-    </>
+            <Link to={href} className="font-bold text-white">{name}</Link>
+          ))}
+        </div>
+
+        <div class="flex md:hidden md:flex space-x-8 mx-6 py-1 xs:mx-3">
+          {/* if you click this expand responsive menu */}
+          <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8" fill="none" viewBox="0 0 24 24" stroke="white">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"/>
+          </svg>
+        </div>
+
+      </nav>
+    </div>
   )
 }
--- frontend/src/components/Stats/Stats.js
+++ frontend/src/components/Stats/Stats.js
@@ -1,73 +1,82 @@
 import React from 'react'
 import { useState } from 'react'
+import './Stats.css';
 
 function Stats({ totalAssets, name }) {      
-    const [html, setHtml] = useState(false);
 
     return (
-        <div class="">
-            <header className="bg-white">
-                <div className="break-normal flex justify-center mx-auto py-6 px-4 text-center">
-                    <h1 className="text-3xl font-bold text-gray-900">{name}</h1>
+        <div class="bg-gray-900">
+            {/* Name of Data Table */}
+            <header className="">
+                <div className="break-normal flex justify-center mx-auto py-4 px-4 text-center">
+                    <h1 className="text-3xl font-bold text-white">{name}</h1>
                 </div>
             </header>
     
-
+            {/* Stats Cards */}
             <div class="grid gap-4 mb-8 md:grid-cols-3 xl:grid-cols-3 px-5 mx-auto">
-                <div class="min-w-0 rounded-lg shadow-xs overflow-hidden bg-white dark:bg-gray-800 border shadow">
-                    <div class="p-4 flex items-center">
-                    <div class="p-3 rounded-full text-green-500 dark:text-green-100 bg-green-100 dark:bg-green-500 mr-4">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="green" class="bi bi-currency-dollar" viewBox="0 0 16 16">
-                            <path d="M4 10.781c.148 1.667 1.513 2.85 3.591 3.003V15h1.043v-1.216c2.27-.179 3.678-1.438 3.678-3.3 0-1.59-.947-2.51-2.956-3.028l-.722-.187V3.467c1.122.11 1.879.714 2.07 1.616h1.47c-.166-1.6-1.54-2.748-3.54-2.875V1H7.591v1.233c-1.939.23-3.27 1.472-3.27 3.156 0 1.454.966 2.483 2.661 2.917l.61.162v4.031c-1.149-.17-1.94-.8-2.131-1.718H4zm3.391-3.836c-1.043-.263-1.6-.825-1.6-1.616 0-.944.704-1.641 1.8-1.828v3.495l-.2-.05zm1.591 1.872c1.287.323 1.852.859 1.852 1.769 0 1.097-.826 1.828-2.2 1.939V8.73l.348.086z"/>
-                        </svg>
-                    </div>
-                    <div>
-                        <p class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">
-                        AVG. MARKET CAP
-                        </p>
-                        <p class="text-lg font-semibold text-gray-700 dark:text-gray-200">
-                        $ --
-                        </p>
-                    </div>
-                    </div>
-                </div>
-                <div class="min-w-0  rounded-lg shadow-xs overflow-hidden bg-white dark:bg-gray-800 border shadow">
-                    <div class="p-4 flex items-center">
-                    <div class="p-3 rounded-full text-blue-500 dark:text-blue-100 bg-blue-100 dark:bg-blue-500 mr-4">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-percent" viewBox="0 0 16 16">
-                            <path d="M13.442 2.558a.625.625 0 0 1 0 .884l-10 10a.625.625 0 1 1-.884-.884l10-10a.625.625 0 0 1 .884 0zM4.5 6a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm0 1a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5zm7 6a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm0 1a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z"/>
-                        </svg>
-                    </div>
-                    <div>
-                        <p class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">
-                        AVG. 24H %
-                        </p>
-                        <p class="text-lg font-semibold text-gray-700 dark:text-gray-200">
-                        $ --
-                        </p>
-                    </div>
+                
+                {/* Card 1 */}
+                <div class="min-w-0 rounded-xl overflow-hidden bg-gray-800 border-gray shadow">
+                    <div class="p-4 flex items-center self-center h-32">
+                        
+                        {/* Card Icon */}
+                        <div class="align-middle p-3 rounded-full bg-gradient-to-b from-green-300 via-blue-300 to-purple-800 shadow">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="black" class="bi bi-currency-dollar" viewBox="0 0 16 16">
+                                <path d="M4 10.781c.148 1.667 1.513 2.85 3.591 3.003V15h1.043v-1.216c2.27-.179 3.678-1.438 3.678-3.3 0-1.59-.947-2.51-2.956-3.028l-.722-.187V3.467c1.122.11 1.879.714 2.07 1.616h1.47c-.166-1.6-1.54-2.748-3.54-2.875V1H7.591v1.233c-1.939.23-3.27 1.472-3.27 3.156 0 1.454.966 2.483 2.661 2.917l.61.162v4.031c-1.149-.17-1.94-.8-2.131-1.718H4zm3.391-3.836c-1.043-.263-1.6-.825-1.6-1.616 0-.944.704-1.641 1.8-1.828v3.495l-.2-.05zm1.591 1.872c1.287.323 1.852.859 1.852 1.769 0 1.097-.826 1.828-2.2 1.939V8.73l.348.086z"/>
+                            </svg>
+                        </div>
+
+                        {/* Card Content */}
+                        <div className="mx-5 inline-block align-middle">
+                            <p class="mb-2 text-sm font-medium text-white">AVG. MARKET CAP</p>
+                            <p class="text-lg font-semibold text-white">$ --</p>
+                        </div>
+                        
                     </div>
                 </div>
-                <div class="min-w-0  rounded-lg shadow-xs overflow-hidden bg-white dark:bg-gray-800 border shadow">
-                    <div class="p-4 flex items-center">
-                    <div class="p-3 rounded-full text-green-500 dark:text-green-100 bg-green-100 dark:bg-green-500 mr-4">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-arrow-up" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/>
-                        </svg>
-                    </div>
-                    <div>
-                        <p class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">
-                        NUMBER OF ASSETS
-                        </p>
-                        <p class="text-lg font-semibold text-gray-700 dark:text-gray-200">
-                        { totalAssets }
-                        </p>
-                    </div>
+
+                {/* Card 2 */}
+                <div class="min-w-0 rounded-xl overflow-hidden bg-gray-800 border-gray shadow">
+                    <div class="p-4 flex items-center self-center h-32">
+                        
+                        {/* Card Icon */}
+                        <div class="align-middle p-3 rounded-full bg-gradient-to-b from-green-300 via-blue-300 to-purple-800 shadow">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="black" class="bi bi-currency-dollar" viewBox="0 0 16 16">
+                                <path d="M4 10.781c.148 1.667 1.513 2.85 3.591 3.003V15h1.043v-1.216c2.27-.179 3.678-1.438 3.678-3.3 0-1.59-.947-2.51-2.956-3.028l-.722-.187V3.467c1.122.11 1.879.714 2.07 1.616h1.47c-.166-1.6-1.54-2.748-3.54-2.875V1H7.591v1.233c-1.939.23-3.27 1.472-3.27 3.156 0 1.454.966 2.483 2.661 2.917l.61.162v4.031c-1.149-.17-1.94-.8-2.131-1.718H4zm3.391-3.836c-1.043-.263-1.6-.825-1.6-1.616 0-.944.704-1.641 1.8-1.828v3.495l-.2-.05zm1.591 1.872c1.287.323 1.852.859 1.852 1.769 0 1.097-.826 1.828-2.2 1.939V8.73l.348.086z"/>
+                            </svg>
+                        </div>
+
+                        {/* Card Content */}
+                        <div className="mx-5 inline-block align-middle">
+                            <p class="mb-2 text-sm font-medium text-white">AVG. 24H %</p>
+                            <p class="text-lg font-semibold text-white">$ --</p>
+                        </div>
+
                     </div>
                 </div>
                 
-            </div>
+                {/* Card 3 */}
+                <div class="min-w-0 rounded-xl overflow-hidden bg-gray-800 border-gray shadow">
+                    <div class="p-4 flex items-center self-center h-32">
+                        
+                        {/* Card Icon */}
+                        <div class="align-middle p-3 rounded-full bg-gradient-to-b from-green-300 via-blue-300 to-purple-800 shadow">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="black" class="bi bi-currency-dollar" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/>
+                            </svg>
+                        </div>
 
+                        {/* Card Content */}
+                        <div className="mx-5 inline-block align-middle">
+                            <p class="mb-2 text-sm font-medium text-white">NUMBER OF ASSETS</p>
+                            <p class="text-lg font-semibold text-white">{ totalAssets }</p>
+                        </div>
+
+                    </div>
+                </div> 
+            </div>
+            
         </div>
     )
 }
